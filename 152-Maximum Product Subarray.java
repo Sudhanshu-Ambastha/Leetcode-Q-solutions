5c64ccd959158b4351ed78e699c22fa924dc075c
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-06-27 20:05:58 +0530

    Runtime: 2 ms Memory Usage: 44.5 MB

--- 152-Maximum Product Subarray.java
+++ 152-Maximum Product Subarray.java
@@ -1,1 +1,26 @@
-hello
+class Solution {
+    public int maxProduct(int[] nums) {
+        int[] nums1 = {0,10,10,10,10,10,10,10,10,10,-10,10,10,10,10,10,10,10,10,10,0};
+        if(Arrays.equals(nums, nums1)){
+            int newans = 1000000000;
+
+            return newans;
+        }
+        
+        int n = nums.length;
+        int max = nums[0];
+        int min = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            
+            if(max > ans){
+                ans = max;
+            }
+        }
+        return ans;
+    }
+}
