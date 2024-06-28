131c1e243af2605c1dd69b583b17b893916a010b
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-06-28 20:11:40 +0530

    Runtime: 0 ms Memory Usage: 41.6 MB

---
+++ 27-Remove Element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        int val = scanner.nextInt();
+
+        Solution solution = new Solution();
+        int result = solution.removeElement(nums, val);
+
+        System.out.println(result);
+    }
+}
