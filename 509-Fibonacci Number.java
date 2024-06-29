653b2f90ed697bd3281092c52df23870dfcdee19
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-06-29 18:11:47 +0530

    Runtime: 0 ms Memory Usage: 39.9 MB

---
+++ 509-Fibonacci Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        for (int i = 2; i <= n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+
+        return b; 
+    }
+}
