125d7e0c51f6b7aadd99a3cb1c8e2917473af750
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-06-29 17:51:06 +0530

    Create 9-Palindrome Number.java

---
+++ 9-Palindrome Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int originalNum = x;
+        int reversedNum = 0;
+
+        while (x > 0) {
+            int remainder = x % 10;
+            reversedNum = reversedNum * 10 + remainder;
+            x = x / 10;
+        }
+
+        return originalNum == reversedNum;
+    }
+}
