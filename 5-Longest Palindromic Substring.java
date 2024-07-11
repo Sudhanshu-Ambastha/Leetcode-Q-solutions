1ad6e085c8ab9e6017f0a30a732101304b06bfad
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-07-11 22:29:43 +0530

    Runtime: 14 ms Memory Usage: 42.1 MB

---
+++ 5-Longest Palindromic Substring.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int start = 0, end = 1;
+
+        for (int i = 0; i < n; i++) {
+            int len1 = expandAroundCenter(s, i, i); // Odd length palindrome
+            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindrome
+
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2 + 1;
+            }
+        }
+
+        return s.substring(start, end);
+    }
+
+    public static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
