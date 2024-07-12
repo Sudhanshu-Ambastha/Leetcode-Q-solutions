60479aa2491e22332c5a20b24e72167d52e980ff
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-07-12 22:23:20 +0530

    Runtime: 0 ms Memory Usage: 41.7 MB

---
+++ 917-Reverse Only Letters.java
@@ -0,0 +1,23 @@
+class Solution { 
+    public String reverseOnlyLetters(String s) { 
+        char[] chars = s.toCharArray(); 
+        int left = 0; 
+        int right = chars.length - 1; 
+        
+        while (left < right) { 
+            if (!Character.isLetter(chars[left])) { 
+                left++; 
+            } else if (!Character.isLetter(chars[right])) { 
+                right--; 
+            } else { 
+                char temp = chars[left]; 
+                chars[left] = chars[right]; 
+                chars[right] = temp; 
+                left++; 
+                right--; 
+            } 
+        } 
+        
+        return new String(chars); 
+    } 
+}
