d6b01c075483d7e43b486f9b73b439b48da7e0bc
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-07-22 20:00:06 +0530

    Runtime: 41 ms Memory Usage: 55.2 MB

---
+++ 0516. Longest Palindromic Subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();  
+        int[][] dp = new int[n][n];  
+
+        for (int i = 0; i < n; i++) {  
+            dp[i][i] = 1;  
+        }  
+
+        for (int len = 2; len <= n; len++) { 
+            for (int i = 0; i <= n - len; i++) {  
+                int j = i + len - 1; 
+                if (s.charAt(i) == s.charAt(j)) {  
+                    dp[i][j] = dp[i + 1][j - 1] + 2;  
+                } else {  
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);  
+                }  
+            }  
+        }  
+ 
+        return dp[0][n - 1]; 
+    }
+}
