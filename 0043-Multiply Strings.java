bb6ebe25dacfc9de2448208c3e71be6dbab33f66
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-08-09 20:16:43 +0530

    Runtime: 2 ms Memory Usage: 42.2 MB

---
+++ 0043-Multiply Strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+       if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int len1 = num1.length();
+        int len2 = num2.length();
+        int[] result = new int[len1 + len2];
+
+        for (int i = len1 - 1; i >= 0; i--) {
+            for (int j = len2 - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = mul + result[i + j + 1];
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
