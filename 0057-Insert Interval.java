e8519062563e265d86b55e4bd790c7b9f6f28e55
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-08-25 21:15:19 +0530

    Runtime: 1 ms Memory Usage: 45 MB

---
+++ 0057-Insert Interval.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
+        
+        // Add all intervals before the new interval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+        
+        // Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
+        
+        // Add all intervals after the new interval
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+        
+        // Convert the result list to a 2D array (matrix)
+        return result.toArray(new int[result.size()][]);
+    }
+}
