95023df587716a31f9457d6f99286d27def9b013
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-08-31 16:10:05 +0530

    Create 0415-Add Strings.java

---
+++ 0415-Add Strings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int carry = 0;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        char[] result = new char[Math.max(num1.length(), num2.length()) + 1];
+        int k = result.length - 1;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int n1 = (i >= 0) ? num1.charAt(i--) - '0' : 0;
+            int n2 = (j >= 0) ? num2.charAt(j--) - '0' : 0;
+            int sum = n1 + n2 + carry;
+            carry = sum / 10;
+            result[k--] = (char) (sum % 10 + '0');
+        }
+
+        // Find the first non-zero character
+        int start = 0;
+        while (start < result.length - 1 && result[start] == '\u0000') {
+            start++;
+        }
+
+        return new String(result, start, result.length - start);
+    }
+}
