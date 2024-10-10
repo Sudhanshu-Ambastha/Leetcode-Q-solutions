fab906697e36794cb2c64cb34be6f9be8979e704
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-10-10 21:31:31 +0530

    Runtime: 58 ms Memory Usage: 48.9 MB

---
+++ 0028-Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle === "") {
+        return 0;
+    }
+
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i;
+        }
+    }
+
+    return -1;
+};
