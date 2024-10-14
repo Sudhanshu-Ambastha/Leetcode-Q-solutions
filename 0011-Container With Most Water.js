a0988c9147b8c7ae31a4957cd87899eb803a3919
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-10-14 21:25:01 +0530

    Runtime: 79 ms Memory Usage: 58.4 MB

---
+++ 0011-Container With Most Water.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+        let width = right - left;
+        let currentHeight = Math.min(height[left], height[right]);
+        let currentArea = width * currentHeight;
+        maxArea = Math.max(maxArea, currentArea);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+};
